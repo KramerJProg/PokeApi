bf4984dc18bbcaa27f63b215e5ecc4f45c2fb0b2
KramerJProg <kramerjensend@gmail.com>
2021-05-14 17:23:19 -0700

    Created basic api client to retrieve Pokemon by name

---
+++ PokeApiCore/PokeApiClient.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PokeApiCore
+{
+    /// <summary>
+    /// Client class to consume PokeApi
+    /// https://pokeapi.co/
+    /// </summary>
+    public class PokeApiClient
+    {
+        static readonly HttpClient client = new HttpClient();
+
+        /// <summary>
+        /// Retireve Pokemon by name
+        /// </summary>
+        /// <exception cref="HttpRequestException"></exception>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public async Task<string> GetPokemonByName(string name)
+        {
+            try
+            {
+                string url = $"https://pokeapi.co/api/v2/pokemon/{name}";
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                return responseBody;
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                throw ex;
+            }
+        }
+
+        public void GetPokemonById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
