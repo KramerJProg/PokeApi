37f38bc7132beed5f181a77d8c05ef198f783ad0
KramerJProg <kramerjensend@gmail.com>
2021-05-16 09:36:20 -0700

    Created Pokedex View Model

---
+++ PokiApiWebsite/Models/PokemonPokedexEntryViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PokiApiWebsite.Models
+{
+    /// <summary>
+    /// Information for a single Pokemon Pokedex Entry
+    /// </summary>
+    public class PokemonPokedexEntryViewModel
+    {
+        public string PokedexImageUrl { get; set; }
+
+        public string Name { get; set; }
+
+        public string Id { get; set; }
+
+        public string Height { get; set; }
+
+        public string Weight { get; set; }
+
+        public IEnumerable<string> MoveList { get; set; }
+    }
+}
