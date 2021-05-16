75ad1739f6786f0562ac6c22972e967c26a41a53
KramerJProg <kramerjensend@gmail.com>
2021-05-15 21:55:03 -0700

    Get JSON response as C# class

--- PokeApiConsole/Program.cs
+++ PokeApiConsole/Program.cs
@@ -9,8 +9,12 @@ namespace PokeApiConsole
         static async Task Main()
         {
             PokeApiClient client = new PokeApiClient();
-            string result = await client.GetPokemonByName("bulbasaur");
-            Console.WriteLine(result);
+            Pokemon result = await client.GetPokemonByName("bulbasaur");
+
+            Console.WriteLine($"Pokemon Id: {result.id}" +
+                              $"\nName: {result.name}" +
+                              $"\nWeight (In Hectograms): {result.weight}" +
+                              $"\nHeight (In Inches): {result.height}");
 
             Console.ReadKey();
         }
